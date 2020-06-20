b1d08819f8b514c9ccd6e484e4c537b879e58870
adndark <adndark@gmail.com>
2020-06-19 22:12:55 -0700

    Format Sound.java class

--- PsychoStrain Game/src/PsychoSystem/Sound.java
+++ PsychoStrain Game/src/PsychoSystem/Sound.java
@@ -1,49 +1,55 @@
-
-//Sound.java (Deprecated)
-
 package PsychoSystem;
 
 //  PsySoft Team 2008
-//       (Manuel Espinoza, Alberto Zorrilla, Guillermo Leon y Arquimides Diaz)
-
-import java.io.*;
-import javax.sound.midi.*;
+//       (Manuel Espinoza, Alberto Zorrilla, Guillermo Leon y Arquimedes Diaz)
+import java.io.FileInputStream;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
 
 public class Sound {
-  
-  boolean playing=false;
-  Sequencer sound;
-  
-  public Sound(String s){    
-    try{
-      sound = MidiSystem.getSequencer();
-          if (sound == null)
-              throw new MidiUnavailableException();
-          sound.open();
-          FileInputStream is = new FileInputStream(s);
-          Sequence mySeq = MidiSystem.getSequence(is);
-          sound.setSequence(mySeq);
-    } catch (Exception e){ System.out.println("Error de Sonido: "+e);}
-  }
-  
-  public void play(){
-    try{
-    playing=true;
-    sound.start();
-    } catch(Exception e){System.out.println("Error de Sonido: "+e);}
-  }
-  
-  public void stop(){
-    try{
-    playing=false;
-    //sound.stop();
-    //sound.close();
-    } catch(Exception e){System.out.println("Error de Sonido: "+e);}
-  }
-  
-  public boolean isPlaying(){
-    return playing;
-  }
-  
-  
+
+    boolean playing = false;
+    Sequencer sound;
+
+    public Sound(String filepath) {
+        try {
+            sound = MidiSystem.getSequencer();
+            if (sound == null) {
+                throw new MidiUnavailableException();
+            }
+            sound.open();
+            FileInputStream is = new FileInputStream(filepath);
+            Sequence mySeq = MidiSystem.getSequence(is);
+            sound.setSequence(mySeq);
+        } catch (Exception e) {
+            System.out.println("Error de Sonido: " + filepath + " " + e
+                    .getMessage());
+        }
+    }
+
+    public void play() {
+        try {
+            playing = true;
+            sound.start();
+        } catch (Exception e) {
+            System.out.println("Error de Sonido: " + e);
+        }
+    }
+
+    public void stop() {
+        try {
+            playing = false;
+            sound.stop();
+            sound.close();
+        } catch (Exception e) {
+            System.out.println("Error de Sonido: " + e);
+        }
+    }
+
+    public boolean isPlaying() {
+        return playing;
+    }
+
 }
